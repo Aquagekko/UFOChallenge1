1711c399063c430bdfa37f61b6d239bf975f0456
Aquagekko <monsterfusco@knights.ucf.edu>
2019-09-22 23:45:21 -0400

    enemeyAI code for following player

---
+++ EnemeyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeyAI : MonoBehaviour
+{
+    public float speed;
+    private Transform target; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectsWithTag("Player").<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
